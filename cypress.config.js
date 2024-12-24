80f49faa040374f83ff46edcff958a0eee8b9391
Erlan <medetbekov_e@iuca.kg>
2024-12-24 12:26:13 +0600

    Добавлены автотесты и отчёты

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,16 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://lalafo.kg',
+        baseUrl: 'https://lalafo.kg', // Укажите базовый URL вашего тестируемого сайта
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            require('cypress-mochawesome-reporter/plugin')(on);
         },
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        reportDir: 'reports', // Папка, где будут сохраняться отчеты
+        overwrite: false,
+        html: true,
+        json: false,
     },
 });
